46a558dd425c3552ba7dd54cf9b711ee0555c2f4
Evan Kahn <ekahn@hmc.edu>
2016-10-25 13:45:14 -0700

    cleaned up code

--- arduino/synth/synth_wrapper.ino
+++ arduino/synth/synth_wrapper.ino
@@ -1,19 +1,36 @@
 #define FILTER_SIZE 10
 #define POT_SAMPLE_RATE 10
-#define INPUT_PIN A10
+#define POT_PIN A10
+
+//Change these values based on minimum and maximum resistance of control potentiometer
+#define POT_MIN 6600
+#define POT_MAX 65536
+
+#define FREQ_MIN 20
+#define FREQ_MAX 12500
 
 //potentiometer values details (w/ 1k pulldown):
 //with test setup: minimum reading = 6600; maximum reading: 65536 (range: 58936)
-//maximum frequency: 12500, minimum frequency: 20 (range: 12480)
 
-int playing = 0;
-int avgs[FILTER_SIZE];
-int avg_ind = 0;
+//constant multiplier maps potentiometer position to frequency range
+const int pot_freq_ratio = (POT_MAX-POT_MIN) / (FREQ_MAX-FREQ_MIN);
+
+//constant offset for lowest possible potentiometer value
+const int pot_offset = POT_MIN * pot_freq_ratio - FREQ_MIN;
+static int playing = 0;
+
+//ring buffer containing samples in moving average
+static int avgs[FILTER_SIZE];
+//pointer offset of oldest buffer contents
+static int avg_ind = 0;
+
+//time between cycles
+static int dt;
 
 AudioControlSGTL5000 codec;
 
 void setup(){
-  pinMode(INPUT_PIN, INPUT);
+  pinMode(POT_PIN, INPUT);
   Serial.begin(115200);
   analogReadResolution(16);
   AudioMemory(18);
@@ -34,11 +51,12 @@ float pot_to_freq(){
     in += avgs[n];
   }
   in /= FILTER_SIZE;
-  float freq = in*12480.0/58936.0-1378.0;
+  float freq = in * pot_freq_ratio - pot_offset;
   return freq;
 }
 
 void loop(){
+  dt = millis();
   if (playing==1){
     if (analogRead(A10) < 200){
       //button released
@@ -48,19 +66,17 @@ void loop(){
       waveform1.frequency(pot_to_freq());
     }
   }else{
-    //button pushed
+    //button pressed
     if (analogRead(A10) > 200){
         AudioNoInterrupts();
         waveform1.begin(0.2, pot_to_freq(), WAVEFORM_SINE);
         envelope1.noteOn();
         AudioInterrupts();
         playing = 1;
-    }else{
-      
     }
   }
-  delay(POT_SAMPLE_RATE);
-  avgs[avg_ind] = analogRead(INPUT_PIN);
-  avg_ind = (avg_ind + 1) % FILTER_SIZE; 
+  avgs[avg_ind] = analogRead(POT_PIN);
+  avg_ind = (avg_ind + 1) % FILTER_SIZE;
+  while (millis() - dt < POT_SAMPLE_RATE) {}
 }
 
