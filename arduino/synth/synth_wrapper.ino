7d64947d35e30be6c07ecb5b2d66e4224179bf36
Evan Kahn <ekahn@hmc.edu>
2016-10-29 17:03:37 -0700

    switched to interrupts over polling

--- arduino/synth/synth_wrapper.ino
+++ arduino/synth/synth_wrapper.ino
@@ -1,10 +1,11 @@
-#define FILTER_SIZE 4
-#define POT_SAMPLE_RATE 4
 #define POT_PIN A10
 
 #define RMIN 23     // minimum neck resistance, in kOhms
 #define RMAX 190    // maximum neck resistance, in kOhms
 #define PULL 62     // size of pull resistor, in kOhms
+#define POLL_NECK_MICROS 6000 // interval between resistor polls (us)
+#define EVENT_LOOP_DT 10 // time between loop() calls (ms)
+#define FILTER_SIZE 16  // size of moving average buffer
 
 //minimum and maximum osc frequencies
 #define FREQ_MIN 160.0
@@ -29,9 +30,11 @@ static int neckscale = 65536 * PULL;
 static int neckscale_2 = 65536 / (RMAX-RMIN);
 
 AudioControlSGTL5000 codec;
+IntervalTimer pot_read_int;
 
 void setup() {
   pinMode(POT_PIN, INPUT);
+  pot_read_int.begin(read_scale_neck, POLL_NECK_MICROS);
   Serial.begin(115200);
   analogReadResolution(16);
   AudioMemory(18);
@@ -49,29 +52,37 @@ void setup() {
 int pot_to_freq() {
   int in = 0;
   int good = FILTER_SIZE;
+  noInterrupts();
   for (int n = 0; n < FILTER_SIZE; n++) {
     if (avgs[n] == 0) {
       good -= 1;
     } else in += avgs[n];
   }
+  interrupts();
   in /= good;
   int freq = (1.0-in/65536.0) * (FREQ_MAX - FREQ_MIN) + FREQ_MIN;
 //  Serial.printf("%d %d\n",in,freq);
   return freq;
 }
 
-int read_scale_neck() {
+void read_scale_neck() {
   // resistance of neck, accounting for voltage division: PULL * (65536-N)/N
   int a = analogRead(A10);
-  if (a < 6000) return 0;
-  return (neckscale/a-PULL-RMIN)*neckscale_2;
+  if (a < 6000) a = 0;
+  else a = (neckscale/a-PULL-RMIN)*neckscale_2;
+  pot_val = a;
+  avgs[avg_ind] = pot_val;
+  avg_ind = (avg_ind + 1) % FILTER_SIZE;
 }
 
 void loop() {
   dt = millis();
+  noInterrupts();
+  int pv = pot_val;
+  interrupts();
   //if button is currently pressed
   if (playing == 1) {
-    if (pot_val == 0) {
+    if (pv == 0) {
       //if button is released
       envelope1.noteOff();
       playing = 0;
@@ -81,7 +92,7 @@ void loop() {
     }
   } else {
     //button pressed
-    if (pot_val != 0) {
+    if (pv != 0) {
       AudioNoInterrupts(); 
       waveform1.begin(0.2, pot_to_freq(), WAVEFORM_SAWTOOTH);
       waveform1.pulseWidth(0.3);
@@ -90,8 +101,5 @@ void loop() {
       playing = 1;
     }
   }
-  pot_val = read_scale_neck();
-  avgs[avg_ind] = pot_val;
-  avg_ind = (avg_ind + 1) % FILTER_SIZE;
-  while (millis() - dt < POT_SAMPLE_RATE) {}
+  while (millis() - dt < EVENT_LOOP_DT) {}
 }
