c15c35979176c3a2c7be44e1a6aadb720ed75a8a
Evan Kahn <ekahn@hmc.edu>
2016-11-13 13:52:31 -0800

    Add line in functionality.

--- arduino/synth/synth_wrapper.ino
+++ arduino/synth/synth_wrapper.ino
@@ -40,10 +40,8 @@ static float freq_max = FREQ_MAX_INIT;
 
 //current octave
 static int octave = 0;
-#define MAX_OCT 3
-#define MIN_OCT -3
-static const int min_octave = -2;
-static const int max_octave = 2;
+#define MAX_OCT 2
+#define MIN_OCT -2
 
 //neck scaling coefficients
 static int neckscale = 65536 * PULL;
@@ -52,6 +50,8 @@ static int neckscale_2 = 65536 / (RMAX-RMIN);
 //frequency coefficients
 static float a_coeff;
 static float b_coeff;
+
+static int glide = 0;
   
 Adafruit_SSD1306 display(OLED_RESET);
 AudioControlSGTL5000 codec;
@@ -65,35 +65,39 @@ void setup() {
   pinMode(BUTTON_RIGHT, INPUT);
   debounce_1.attach(BUTTON_LEFT);
   debounce_2.attach(BUTTON_RIGHT);
-  debounce_1.interval(5);
-  debounce_2.interval(5);
+  debounce_1.interval(50);
+  debounce_2.interval(50);
   pinMode(POT_PIN, INPUT);
   pot_read_int.begin(read_scale_neck, POLL_NECK_MICROS);
   Serial.begin(115200);
   analogReadResolution(16);
-  AudioMemory(18);
+  AudioMemory(20);
   AudioProcessorUsageMaxReset();
   AudioMemoryUsageMaxReset();
   codec.enable();
-  codec.volume(0.5);
+  codec.volume(1);
+  codec.lineOutLevel(31);
+  codec.lineInLevel(0);
   envelope1.attack(0);
   envelope1.hold(0);
   envelope1.decay(0);
   envelope1.sustain(1);
   envelope1.release(1);
-  mixer1.gain(1,2);
-  mixer1.gain(2,.3);
+  mixer1.gain(1,.5);
+  mixer1.gain(2,.5);
   
-  mixer2.gain(1,2);
-  mixer2.gain(2,.3);
+  mixer2.gain(1,.5);
+  mixer2.gain(2,.5);
   change_octave(octave);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
   disp_update();
+  waveform1.begin(0.4, pot_to_freq(), WAVEFORM_SQUARE);
+  waveform1.pulseWidth(0.5);
 }
 
 void change_octave(int oct){
   octave += oct;
-  if (octave < MAX_OCT && octave > MIN_OCT){
+  if (octave <= MAX_OCT && octave >= MIN_OCT){
     freq_min *= (pow(2,oct));
     freq_max *= (pow(2,oct));
     //coefficient for inverse freq scaling
@@ -104,13 +108,13 @@ void change_octave(int oct){
 int pot_to_freq() {
   int in = 0;
   int good = FILTER_SIZE;
-//  noInterrupts();
+  noInterrupts();
   for (int n = 0; n < FILTER_SIZE; n++) {
     if (avgs[n] == 0) {
       good -= 1;
     } else in += avgs[n];
   }
-//  interrupts();
+  interrupts();
   in /= good;
   int freq = 1.0/(a_coeff * in / 65536.0 + b_coeff);
   
@@ -145,15 +149,17 @@ void loop() {
   debounce_2.update();
   
   dt = millis();
-//  noInterrupts();
+  noInterrupts();
   int pv = pot_val;
-//  interrupts();
+  interrupts();
   //if button is currently pressed
   if (playing == 1) {
     if (pv == 0) {
       //if button is released
       envelope1.noteOff();
       playing = 0;
+      noInterrupts();
+      interrupts();
     } else {
       //if button is held down, update frequency with moving average
       waveform1.frequency(pot_to_freq());
@@ -169,11 +175,10 @@ void loop() {
       }
     //button pressed
     if (pv != 0) {
-//      AudioNoInterrupts(); 
-      waveform1.begin(0.4, pot_to_freq(), WAVEFORM_SAWTOOTH);
-      waveform1.pulseWidth(0.3);
+      AudioNoInterrupts(); 
+      waveform1.frequency(pot_to_freq());
       envelope1.noteOn();
-//      AudioInterrupts();
+      AudioInterrupts();
       playing = 1;
     }
   }
