e4ee972b181e3cc93b52a141fb4d02b42540952c
Evan Kahn <ekahn@hmc.edu>
2016-11-13 18:01:50 -0800

    Begin working on menu framework.

--- arduino/synth/synth_wrapper.ino
+++ arduino/synth/synth_wrapper.ino
@@ -1,5 +1,4 @@
 #include <Wire.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Bounce2.h>
 
@@ -12,15 +11,18 @@
 #define RMIN 23     // minimum neck resistance, in kOhms
 #define RMAX 190    // maximum neck resistance, in kOhms
 #define PULL 62     // size of pull resistor, in kOhms
-#define POLL_NECK_MICROS 8000 // interval between resistor polls (us)
+#define POLL_NECK_MICROS 6000 // interval between resistor polls (us)
 #define EVENT_LOOP_DT 10 // time between loop() calls (ms)
-#define FILTER_SIZE 8  // size of moving average buffer
+#define FILTER_SIZE 12  // size of moving average buffer
 
 //minimum and maximum osc frequencies
 #define FREQ_MIN_INIT 160.0
 #define FREQ_MAX_INIT 1447.0
 
 static int playing = 0;
+
+//hold both cheeks during startup to go to patch edit mode
+static uint8_t edit_mode = 0;
 
 //ring buffer containing samples in moving average
 static int avgs[FILTER_SIZE];
@@ -50,8 +52,6 @@ static int neckscale_2 = 65536 / (RMAX-RMIN);
 //frequency coefficients
 static float a_coeff;
 static float b_coeff;
-
-static int glide = 0;
   
 Adafruit_SSD1306 display(OLED_RESET);
 AudioControlSGTL5000 codec;
@@ -63,13 +63,18 @@ Bounce debounce_2 = Bounce();
 void setup() {
   pinMode(BUTTON_LEFT, INPUT);
   pinMode(BUTTON_RIGHT, INPUT);
+
+  //enter edit mode if both buttons are down at startup
+  if (digitalRead(BUTTON_LEFT) == 1 && digitalRead(BUTTON_RIGHT) == 1){
+    edit_mode = 1;
+  }
+  
   debounce_1.attach(BUTTON_LEFT);
   debounce_2.attach(BUTTON_RIGHT);
   debounce_1.interval(50);
   debounce_2.interval(50);
   pinMode(POT_PIN, INPUT);
   pot_read_int.begin(read_scale_neck, POLL_NECK_MICROS);
-  Serial.begin(115200);
   analogReadResolution(16);
   AudioMemory(20);
   AudioProcessorUsageMaxReset();
@@ -78,20 +83,20 @@ void setup() {
   codec.volume(1);
   codec.lineOutLevel(31);
   codec.lineInLevel(0);
-  envelope1.attack(0);
-  envelope1.hold(0);
-  envelope1.decay(0);
-  envelope1.sustain(1);
-  envelope1.release(1);
+  envelope1.attack(5);
+  envelope1.hold(30);
+  envelope1.decay(30);
+  envelope1.sustain(.5);
+  envelope1.release(50);
   mixer1.gain(1,.5);
   mixer1.gain(2,.5);
-  
   mixer2.gain(1,.5);
   mixer2.gain(2,.5);
   change_octave(octave);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  disp_setup();
   disp_update();
-  waveform1.begin(0.4, pot_to_freq(), WAVEFORM_SQUARE);
+  waveform1.begin(1, pot_to_freq(), WAVEFORM_SQUARE);
   waveform1.pulseWidth(0.5);
 }
 
@@ -132,15 +137,21 @@ void read_scale_neck() {
   avg_ind = (avg_ind + 1) % FILTER_SIZE; 
 }
 
+void disp_setup(){
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+}
+
 void disp_update(){
   display.clearDisplay();
-  display.setTextSize(.5);
-  display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.setTextSize(2);
-  display.print("Octave: ");
-  if (octave >= 0) display.print("+");
-  display.println(octave);
+  if (edit_mode){
+    display.print("Edit Mode");
+  }else{
+    display.print("Octave: ");
+    if (octave >= 0) display.print("+");
+    display.println(octave);
+  }
   display.display();
 }
 
@@ -165,13 +176,15 @@ void loop() {
       waveform1.frequency(pot_to_freq());
     }
   } else {
-      if (debounce_1.fell()){
-        change_octave(-1);
-        disp_update();
-      }
-      if (debounce_2.fell()){
-        change_octave(1);
-        disp_update();
+      if (!edit_mode){
+        if (debounce_1.fell()){
+          change_octave(-1);
+          disp_update();
+        }
+        if (debounce_2.fell()){
+          change_octave(1);
+          disp_update();
+        }
       }
     //button pressed
     if (pv != 0) {
