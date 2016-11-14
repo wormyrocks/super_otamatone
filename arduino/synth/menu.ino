2537bb5877169a66ef304802fc1ca3edfffd0935
Evan Kahn <ekahn@hmc.edu>
2016-11-14 01:49:47 -0800

    Did my forward declarations properly.

--- arduino/synth/menu.ino
+++ arduino/synth/menu.ino
@@ -59,6 +59,7 @@ MenuSystem ms(my_renderer);
 void on_item1_selected(MenuItem* p_menu_item);
 void on_item2_selected(MenuItem* p_menu_item);
 void on_item3_selected(MenuItem* p_menu_item);
+void on_item4_selected(MenuItem* p_menu_item);
 
 MenuItem mm_mi1("Level 1 Item 1 (Item)", &on_item1_selected);
 MenuItem mm_mi2("Level 1 Item 2 (Item)", &on_item2_selected);
